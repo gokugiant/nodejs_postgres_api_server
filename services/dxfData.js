fe1b2d50692e7f4b21dfea2cb1e3d5a9621647f7
root <root@foxcloud.de>
2021-12-09 16:09:34 +0100

    alter sql statement

--- services/dxfData.js
+++ services/dxfData.js
@@ -4,11 +4,12 @@ const config = require('../config');
 
 async function getDXF() {
     const rows = await db.query(
-        'select * from public.building_tbl \n' +
+        'select objectid, objecttype_tbl.name, json from public.building_tbl \n' +
                 'left join level_tbl on level_tbl.buildingid = building_tbl.buildingid\n' +
                 'left join layer_tbl on level_tbl.levelid = layer_tbl.levelid\n' +
                 'left join object_tbl on object_tbl.layerid = layer_tbl.layerid\n' +
-                'left join objecttype_tbl on object_tbl.objecttypeid = objecttype_tbl.objecttypeid;'
+                'left join objecttype_tbl on object_tbl.objecttypeid = objecttype_tbl.objecttypeid\n' +
+	    	'WHERE object_tbl.objecttypeid IS NOT NULL;'
     );
     const data = helper.emptyOrRows(rows);
 
